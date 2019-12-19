46f11e3dd7fcaedef5ef22ccd2f73e315d6376a6
Mamun Miah <Mamun8805@outlook.com>
2019-12-18 19:58:22 -0500

    adding new file

---
+++ Allahisgreat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Our individual or collective success or failure prosperity or bankruptcy peace and fulfillment or suffering and destruction depend upon one’s reverence and alignment with Allah."
